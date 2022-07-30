a1718d4f005eb09ddf9017cc61fd5b6742b0c20b
alphanse <alphanse98@gmail.com>
2022-07-30 10:20:51 +0530

    add

--- files/css/ap.js
+++ files/css/ap.js
@@ -1,11 +1,21 @@
 let navBtn = document.querySelector(".navBtn"),
   navBar = document.querySelector(".nav-bar"),
   overLay = document.querySelector(".overLay"),
-  navItems = document.querySelectorAll(".navItems");
+  navItems = document.querySelectorAll(".navItems"),
+  
+ line1 = document.querySelector('.line1');
+ line2 = document.querySelector('.line2');
+ line3 = document.querySelector('.line3');
+  
 
   let action = () => {
-    navBar.classList.toggle("hidden");
+    navBar.classList.toggle("display");
     overLay.classList.toggle("hidden");
+    // navBar.style.transform = "translate(0%)" 
+    line1.classList.toggle("toggle1");
+    line2.classList.toggle("toggle2");
+    line3.classList.toggle("toggle3");
+    // console.log(navBtn)
   };
 
 navBtn.addEventListener("click", function () {
